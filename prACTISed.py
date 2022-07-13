d43f07617cbca03ff8427c345ce92cbad867ef62
JessLat <108759671+JessLat@users.noreply.github.com>
2022-07-12 20:00:19 -0400

    Formatting returned graphs and values
    Superimposing separagrams (all runs for a concentration, one run for each concentration)
    Writing summary data into a new sheet of Excel file
    Option to specify which concentration used to calculate peak time and time window (additions necessary to idealinputs.xlsx at B9)

--- prACTISed.py
+++ prACTISed.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-                 
 
 # ACTIS - Kd Determination Program
-# July 8, 2022
+# July 12, 2022
 
 #20220708 JL    NOTE: Program no longer generates temporary Excel files
 #20220711 JL    NOTE: Verbose statements added, all graphs returned at end of script
+#20220712 JL    NOTE: Added: export data to inputted Excel, superimposing separagrams and option to choose concentration to calculate peak signal
 
 # This program extracts ACTIS titration data in a Microsoft Excel file (.xlsx) organized in a particular way*,
 # and determines the signal (average peak height within a detection window) for each concentration and a corresponding
@@ -47,6 +48,7 @@ parser.add_argument('-v', '--verbose', help = 'prints detailed output while anal
 
 args = vars(parser.parse_args())
 
+
 ## Part 3 - Locating the raw data file and establishing important inputs
 fileName = args['inputfile'][0]                         # It's a list but we can only work with one file                           
 if not pathlib.Path(fileName).is_file():
@@ -68,6 +70,7 @@ if "Inputs" in inputBook.sheetnames:
     numberOfConcs = (idealSheet.cell(3,2).value)
     injectionTime = int(idealSheet.cell(5,2).value)
     ligandConc = float(idealSheet.cell(7,2).value)
+    windowCalcConc = idealSheet.cell(9,2).value
 
 elif "Inputs" not in inputBook.sheetnames:  
         sys.exit("Inputfile Formatting Error: The script expects inputdata.xlsx to be in a certain format, see provided idealinputs.xlsx as an example.") 
@@ -84,7 +87,22 @@ forDF = [concentration,signal,stddev,relstddev,Rvalue,Rstddev]
 DFnames = ["Conc (µM)","Avg Signal","Std Dev","Rel Std Dev","R value","R Std Dev"]
 
 
-## Part 3 - Calculating signal information for each concentration
+# Using first run at specified concentration to calculate peak time and time window
+data = pd.read_excel(fileName, str(windowCalcConc) + " µM", engine='openpyxl')
+data = data.dropna(how='all')
+xvalues = data['raw time']
+yvalues = data['Experiment 1']
+peakSignal = max(yvalues[xvalues>=injectionTime])
+peakIndex = yvalues[yvalues == peakSignal].index
+peakTime = xvalues[peakIndex]
+
+if args['verbose']:
+        # Prints the peak signal value and the corresponding time
+        print("Peak signal: %.4f RFU at %.4f seconds" % (peakSignal, peakTime))
+        exit
+
+
+## Part 4 - Calculating signal information for each concentration and generating separagram graphs
 for x in range(1,int(numberOfConcs)+1):         
     conc1 = idealSheet.cell(x,5).value
     numberOfRuns = int(idealSheet.cell(x,7).value)
@@ -98,8 +116,8 @@ for x in range(1,int(numberOfConcs)+1):
 
 
     # Calculate background signals for each run
-    for runNumber in range(1, numberOfRuns+1):  
-
+    for runNumber in range(1, numberOfRuns+1):
+        
         if args['verbose']:
             # Prints the concentration and run number
             print("-------- Concentration %.1f µM -------- Experimental Run %d  -------- " % (conc1, runNumber))
@@ -113,26 +131,13 @@ for x in range(1,int(numberOfConcs)+1):
         background_average = np.average(background_yvalues)
         background_stdev = np.std(background_yvalues)
 
-
         if args['verbose']:
             # Prints the number of background signals with average and standard deviation
             print("Background signal (first %d values): %.4f±%.4f RFU" % (len(background_yvalues), background_average, background_stdev))
             exit
+            
 
-
-        # First run at lowest concentration used to calculate peak time and time window
-        if x==1 and runNumber==1:
-                peakSignal = max(yvalues[xvalues>=injectionTime])
-                peakIndex = yvalues[yvalues == peakSignal].index
-                peakTime = xvalues[peakIndex]
-
-                if args['verbose']:
-                        # Prints the peak signal value and the corresponding time
-                        print("Peak signal: %.4f RFU at %.4f seconds" % (peakSignal, peakTime))
-                        exit
-
-
-        # Set time window parameters and determine the average signal within window for each run             
+        # Set time window parameters using peak time and determine the average signal within window for each run             
         windowLow = float(peakTime - (percentage * peakTime))
         windowHigh = float(peakTime + (percentage * peakTime))
         windowIndex = xvalues.between(windowLow,windowHigh)
@@ -146,16 +151,21 @@ for x in range(1,int(numberOfConcs)+1):
                         exit
 
 
-        # Graph the signal for each run and show time window
-        #plt.scatter(xvalues, yvalues)
-        #plt.xlabel('Propagation time (s)')
-        #plt.ylabel('Fluorescence signal (RFU)')
-        #plt.title("Separagram for %.1f µM Experimental Run %d" % (conc1, runNumber))
-        #plt.xlim(xmin=0)
-        #plt.vlines(windowLow, 0, (float(yvalues.max()+ 0.05*float(yvalues.max()))), linestyles='dashed',color='black')
-        #plt.vlines(windowHigh, 0, (float(yvalues.max()+ 0.05*float(yvalues.max()))), linestyles='dashed',color='black')
-        #graphs.append(plt.figure())
-        #####plt.show(block=False)
+        # Graph the signal for each run and with time window indicated
+        plt.plot(xvalues, yvalues, label='Run %d' % (runNumber))
+        plt.xlabel('Propagation time (s)')
+        plt.ylabel('Fluorescence signal (RFU)')
+        plt.title("Experimental runs for %.1f µM" % (conc1))
+        plt.legend()
+        plt.xlim(xmin=0)
+        plt.vlines(windowLow, 0, (float(yvalues.max()+ 0.05*float(yvalues.max()))), linestyles='dashed',color='black')
+        plt.vlines(windowHigh, 0, (float(yvalues.max()+ 0.05*float(yvalues.max()))), linestyles='dashed',color='black')
+
+    # Appending a figure with all experimental runs for concentration            
+    #plt.savefig(str(conc1)+ "µM.jpeg")         # save separagram graphs
+    graphs.append(plt.figure())                 
+    plt.clf()
+
 
     # Calculating average signal for each concentration, stdev and relative stdev        
     avgSigConc = np.average(avgSigsRun)
@@ -166,19 +176,37 @@ for x in range(1,int(numberOfConcs)+1):
             # Prints the average signal per concentration with standard deviation
             print("Average signal %.1f µM: %.4f±%.4f RFU" % (conc1, avgSigConc, avgSigConc_stdev))
             exit
-
+            
     concentration.append(conc1)
     signal.append(avgSigConc)    
     stddev.append(avgSigConc_stdev)
     relstddev.append(avgSigConc_relstdev)
+
     
-#if args['verbose']:
-            # Average signal per concentration with standard deviation and relative standard deviation
-            #df = pd.DataFrame (forDF).transpose()
-            #df.columns = DFnames
-            #print(df)
-            #exit
-    
+# Graphing separagrams for the first run for every concentration
+for x in range(1,int(numberOfConcs)+1):         
+    conc1 = idealSheet.cell(x,5).value
+
+    # Reading in the whole data frame (= all runs for one particular concentration) and dropping all lines that are blank, i.e. that would produce "NaN"s
+    data = pd.read_excel(fileName, str(conc1) + " µM", engine='openpyxl')
+    data = data.dropna(how='all')
+    xvalues = data['raw time']
+    yvalues = data['Experiment 1']
+
+    plt.plot(xvalues, yvalues, label='%.1f µM' % (conc1))
+    plt.xlabel('Propagation time (s)')
+    plt.ylabel('Fluorescence signal (RFU)')
+    plt.title('First experimental run for each concentration')
+    plt.legend()
+    plt.xlim(xmin=0)
+    plt.vlines(windowLow, 0, (float(yvalues.max()+ 0.05*float(yvalues.max()))), linestyles='dashed',color='black')
+    plt.vlines(windowHigh, 0, (float(yvalues.max()+ 0.05*float(yvalues.max()))), linestyles='dashed',color='black') 
+        
+# Appending a figure with first experimental run for all concentrations
+#plt.savefig("allconcentration.jpeg")           # save separagram graph
+graphs.append(plt.figure())                     
+plt.clf()
+
 
 ## Part 5 - Calculate R values and standard deviation of R values for each concentration
 LowProt_sig = signal[0]
@@ -229,8 +257,8 @@ plt.ylabel('R')
 plt.xlabel('Concentration of BSA (μM)')
 plt.legend()
 plt.xscale("log")
+#plt.savefig("bindingisotherm.jpeg")           # save binding isotherm graph
 graphs.append(plt.figure())
-#####plt.show(block=False)
 
 # Statistics
 residuals = Rvalue - LevenMarqu(concentration, *popt)
@@ -244,6 +272,28 @@ chiSquared = sum((((Rvalue - LevenMarqu(concentration, *popt))**2) / LevenMarqu(
 print("Kd: %.4f ± %.4f μM" % (popt,error))
 print("R² %.4f" % (r_squared))
 print("χ²: %.4f" % (chiSquared))
+
+
+## Part 7 - Returning summary data and graphs
+# Summary dataframe of average signal per concentration with standard deviation, relative standard deviation, R value and standard deviation
+df = pd.DataFrame (forDF).transpose()
+df.columns = DFnames
+
+# Create new output sheet in input Excel file with summary data and Kd, R² and χ²
+writer = pd.ExcelWriter(fileName, engine = 'openpyxl')
+writer.book = inputBook
+df.to_excel(writer, sheet_name = "Outputs", index=False)        # does not overwrite if a sheet named Outputs already exists
+writer.save()
+writer.close()
+outputSheet = inputBook["Outputs"]
+outputSheet["H1"] = "Kd"
+outputSheet["I1"] = "%.4f ± %.4f μM" % (popt,error)
+outputSheet["H2"] = "R²"
+outputSheet["I2"] = "%.4f" % (r_squared)
+outputSheet["H3"] = "χ²"
+outputSheet["I3"] = "%.4f" % (chiSquared)
+inputBook.save(fileName)
+
 
 # Testing script execution time
 end = time.time()
